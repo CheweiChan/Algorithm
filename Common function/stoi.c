b6f0a1b1048bb7b198be0348240e7d12128b3400
Chewei <31846431+CheweiChan@users.noreply.github.com>
2019-04-02 01:19:20 +0800

    Update stoi.c

--- Common function/stoi.c
+++ Common function/stoi.c
@@ -11,7 +11,7 @@ typedef enum
 
 bool _isdigit(char c)
 {
-	return (c >= '0' && c <= '9');
+    return (c >= '0' && c <= '9');
 }
 
 static int stoi(char* str)
@@ -20,39 +20,39 @@ static int stoi(char* str)
     int n = 0, sign = 0;
 
     while (*str == ' ' || *str == '\t'|| *str == '\n') 
-	{
+    {
         str++;
     }
 
     for (s = str; *s != '\0'; s++) 
-	{
+    {
         if (_isdigit(*s)) 
-		{
+	{
             int d = *s - '0';
             n = n * 10 + d;
             if (sign) 
-			{
+	    {
                 if (-n <MIN)
-				{
+		{
                     n = MIN;
                     break;
                 }
-            } else 
-			{
+            } 
+	    else 
+	    {
                 if (n >MAX)
-				{
+		{
                     n = MAX;
                     break;
                 }
             }
         } 
-		else if (*s == '-' && _isdigit(*(s + 1))) 
+	else if (*s == '-' && _isdigit(*(s + 1))) 
             sign = 1;
         else if (*s == '+' && _isdigit(*(s + 1))) 
             sign = 0;
-		else 
+	else 
             break;
-
     }
 
     return sign ? -n : n;   
@@ -64,6 +64,4 @@ int main(void)
 	int num;
 	num = stoi("-19999999");
 	printf("\n%d\n",num);
-	
-	
 }
